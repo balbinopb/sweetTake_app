cf0ec738c3f7334e4d552e192565adc956573a07
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-11-01 14:37:18 +0700

    feat: add and implement bacekend to register view

---
+++ lib/app/modules/register/views/register_view.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../../../constants/app_colors.dart';
+import '../../../widgets/custom_textfield.dart';
+import '../controllers/register_controller.dart';
+
+class RegisterView extends GetView<RegisterController> {
+  const RegisterView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'sweetTake',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontFamily: 'sansitaOne',
+                  color: AppColors.primary,
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Create Your Account',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'sansitaOne',
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.primary,
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Start your journey toward balanced and healthy sugar levels',
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(height: 40),
+
+              CustomTextfield(
+                textController: controller.usernameC,
+                labelText: "Username",
+              ),
+
+              SizedBox(height: 16),
+              
+              CustomTextfield(
+                textController: controller.emailC,
+                labelText: "Email",
+              ),
+              
+              SizedBox(height: 16),
+              
+              CustomTextfield(
+                textController: controller.passwordC,
+                labelText: "Password",
+              ),
+              
+              SizedBox(height: 16),
+
+              SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {
+                    controller.login();
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(AppColors.primary),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  child: Text("Login", style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
