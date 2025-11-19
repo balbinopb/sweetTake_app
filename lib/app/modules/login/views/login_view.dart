a0b8cf867ebb8a4e07693a33758b1f74cc9a5d7c
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-11-19 21:00:38 +0700

    update login view and controller

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -12,103 +12,111 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'sweetTake',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontFamily: 'sansitaOne',
-                  color: AppColors.primary,
-                ),
-              ),
-              SizedBox(height: 50),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Welcome back !',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'sansitaOne',
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.primary,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight:
+                  MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top -
+                  MediaQuery.of(context).padding.bottom,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 50),
+                  Text(
+                    'sweetTake',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontFamily: 'sansitaOne',
+                      color: AppColors.primary,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Log in to continue tracking your balanced and healthy sugar levels',
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.grey),
-              ),
-              SizedBox(height: 16),
+                  const SizedBox(height: 50),
 
-              CustomTextfield(
-                textController: controller.emailC,
-                labelText: "Email",
-              ),
-              SizedBox(height: 16),
-              CustomTextfield(
-                textController: controller.passwordC,
-                labelText: "Password",
-              ),
-              SizedBox(height: 40),
-
-              SizedBox(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () {
-                    controller.login();
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(AppColors.primary),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                  // your content
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Welcome back !',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'sansitaOne',
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.primary,
                       ),
                     ),
                   ),
-                  child: Text("Login", style: TextStyle(color: Colors.white)),
-                ),
-              ),
-
-              SizedBox(height: 16),
-
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Forgot your password?",
-                  style: TextStyle(color: AppColors.primary),
-                ),
-              ),
-
-              // SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                  const SizedBox(height: 8),
                   Text(
-                    "Don’t have account? ",
-                    style: TextStyle(color: AppColors.primary),
+                    'Log in to continue tracking your balanced and healthy sugar levels',
+                    style: TextStyle(color: Colors.grey),
                   ),
+                  const SizedBox(height: 16),
+
+                  CustomTextfield(
+                    textController: controller.emailC,
+                    labelText: "Email",
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextfield(
+                    textController: controller.passwordC,
+                    labelText: "Password",
+                  ),
+                  const SizedBox(height: 40),
+
+                  SizedBox(
+                    width: double.infinity,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: controller.login,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+
                   TextButton(
-                    onPressed: () {
-                      Get.toNamed(Routes.REGISTER);
-                    },
+                    onPressed: () {},
                     child: Text(
-                      "Register",
+                      "Forgot your password?",
                       style: TextStyle(color: AppColors.primary),
                     ),
                   ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don’t have account? ",
+                        style: TextStyle(color: AppColors.primary),
+                      ),
+                      TextButton(
+                        onPressed: () => Get.toNamed(Routes.REGISTER),
+                        child: Text(
+                          "Register",
+                          style: TextStyle(color: AppColors.primary),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const Spacer(),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
