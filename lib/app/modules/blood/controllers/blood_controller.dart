038d16190d2e013f76ce409d42e59ad050d7b358
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-11-21 15:36:54 +0700

    update controllers

--- lib/app/modules/blood/controllers/blood_controller.dart
+++ lib/app/modules/blood/controllers/blood_controller.dart
@@ -1,23 +1,5 @@
 import 'package:get/get.dart';
 
 class BloodController extends GetxController {
-  //TODO: Implement BloodController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
